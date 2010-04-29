4e6cdba44cdae94b4fc25439b6a23fa8fada8e1c
jimmarino <jim.marino@gmail.com>
2010-04-29 16:55:00 +0000

    remove component type

--- test-implementation-wsdl/src/main/java/org/fabric3/tests/implementation/wsdl/model/TestWsdlComponentType.java
+++ test-implementation-wsdl/src/main/java/org/fabric3/tests/implementation/wsdl/model/TestWsdlComponentType.java
@@ -37,13 +37,13 @@
 */
 package org.fabric3.tests.implementation.wsdl.model;
 
-import org.fabric3.model.type.component.ComponentType;
+import org.fabric3.model.type.component.AbstractComponentType;
 
 /**
  * @version $Rev$ $Date$
  */
-public class TestWsdlComponentType extends ComponentType {
+public class TestWsdlComponentType extends AbstractComponentType {
     private static final long serialVersionUID = -2504613183045200259L;
 
-    
+
 }
