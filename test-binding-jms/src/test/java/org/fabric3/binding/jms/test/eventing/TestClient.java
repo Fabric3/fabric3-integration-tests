00a86527614f7af2cdb068269d79b0f1a0cb3693
jimmarino <jim.marino@gmail.com>
2010-05-05 13:03:42 +0000

    add eventing integration tests

---
+++ test-binding-jms/src/test/java/org/fabric3/binding/jms/test/eventing/TestClient.java
@@ -0,0 +1,25 @@
+package org.fabric3.binding.jms.test.eventing;
+
+import junit.framework.TestCase;
+import org.oasisopen.sca.annotation.Reference;
+
+import org.fabric3.tests.binding.harness.eventing.TestConsumer;
+import org.fabric3.tests.binding.harness.eventing.TestProducer;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class TestClient extends TestCase {
+
+    @Reference
+    protected TestProducer producer;
+
+    @Reference
+    protected TestConsumer consumer;
+
+    public void testProduce() throws Exception {
+        consumer.setWaitCount(2);
+        producer.produce("message");
+        consumer.waitOnEvents();
+    }
+}
