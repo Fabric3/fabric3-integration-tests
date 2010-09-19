14a8c1993042484be6d7613e02efcbe161724207
jimmarino <jim.marino@gmail.com>
2010-09-19 15:29:21 +0000

    add security tests

--- test-binding-rs/src/test/java/tests/rs/SecureTestClient.java
+++ test-binding-rs/src/test/java/tests/rs/SecureTestClient.java
@@ -16,18 +16,27 @@
  */
 package tests.rs;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.UriBuilder;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.client.filter.ClientFilter;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import junit.framework.TestCase;
 import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
 import org.osoa.sca.annotations.Property;
+
+import org.fabric3.tests.rs.security.UsernamePasswordToken;
 
 /**
  * @version $Rev$ $Date$
@@ -35,7 +44,10 @@ import org.osoa.sca.annotations.Property;
 public class SecureTestClient extends TestCase {
 
     @Property
+    protected String authentication;
+    @Property
     protected String baseUri;
+
     UriBuilder uri;
     private Client client;
 
@@ -75,6 +87,29 @@ public class SecureTestClient extends TestCase {
 
     public void testBasicAuth() {
         client.addFilter(new HTTPBasicAuthFilter("foo", "bar"));
+        WebResource resource = client.resource(uri.path("").build());
+        resource.type(MediaType.APPLICATION_JSON).get(String.class);
+    }
+
+    public void testFabricLogin() {
+        client.addFilter(new ClientFilter() {
+            private List<NewCookie> cookies = new ArrayList<NewCookie>();
+
+            @Override
+            public ClientResponse handle(ClientRequest cr) throws ClientHandlerException {
+                for (NewCookie cookie : cookies) {
+                    cr.getHeaders().putSingle("Cookie", cookie.getName() + "=" + cookie.getValue());
+                }
+                ClientResponse resp = getNext().handle(cr);
+                for (NewCookie cookie : resp.getCookies()) {
+                    cookies.add(cookie);
+                }
+                return resp;
+            }
+        });
+        UriBuilder authBuilder = UriBuilder.fromUri(authentication);
+        WebResource authResource = client.resource(authBuilder.path("").build());
+        authResource.type(MediaType.APPLICATION_JSON_TYPE).post(new UsernamePasswordToken("foo", "bar"));
         WebResource resource = client.resource(uri.path("").build());
         resource.type(MediaType.APPLICATION_JSON).get(String.class);
     }
