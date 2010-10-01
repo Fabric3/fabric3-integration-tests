d98f2ace87630e52ce8fab2f283cc4d8ce7d92ee
jimmarino <jim.marino@gmail.com>
2010-10-01 20:40:05 +0000

    change package

--- test-implementation-wsdl/src/main/java/org/fabric3/tests/implementation/wsdl/generator/TestWsdlImplementationGenerator.java
+++ test-implementation-wsdl/src/main/java/org/fabric3/tests/implementation/wsdl/generator/TestWsdlImplementationGenerator.java
@@ -52,7 +52,7 @@ import org.fabric3.spi.model.physical.PhysicalSourceDefinition;
 import org.fabric3.spi.model.physical.PhysicalTargetDefinition;
 import org.fabric3.spi.model.physical.PhysicalConnectionSourceDefinition;
 import org.fabric3.spi.model.physical.PhysicalConnectionTargetDefinition;
-import org.fabric3.spi.policy.EffectivePolicy;
+import org.fabric3.spi.generator.policy.EffectivePolicy;
 import org.fabric3.tests.implementation.wsdl.model.TestWsdlImplementation;
 import org.fabric3.tests.implementation.wsdl.provision.PhysicalWsdlComponentDefinition;
 import org.fabric3.tests.implementation.wsdl.provision.PhysicalWsdlSourceDefinition;
