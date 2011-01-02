47af15270e10f8ebdadaa3bf104e07459ec56783
jimmarino <jim.marino@gmail.com>
2011-01-02 22:45:59 +0000

    optimize imports

--- test-capabilities-extension/src/main/java/org/fabric3/tests/capabilities/HelloService.java
+++ test-capabilities-extension/src/main/java/org/fabric3/tests/capabilities/HelloService.java
@@ -21,7 +21,6 @@ package org.fabric3.tests.capabilities;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
