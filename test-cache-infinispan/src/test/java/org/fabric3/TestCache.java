56632185b0974e00c15320e3f77df89c8a07c910
michalcaposvn <noreply@fabric3.org>
2011-02-07 13:46:16 +0000

    - [infinispan] added integration test

---
+++ test-cache-infinispan/src/test/java/org/fabric3/TestCache.java
@@ -0,0 +1,30 @@
+package org.fabric3;
+
+import junit.framework.TestCase;
+import org.fabric3.api.annotation.Resource;
+import org.fabric3.api.annotation.scope.Scopes;
+import org.oasisopen.sca.annotation.EagerInit;
+import org.oasisopen.sca.annotation.Init;
+import org.oasisopen.sca.annotation.Scope;
+
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+@Scope(Scopes.COMPOSITE)
+public class TestCache extends TestCase {
+
+    @Resource(name = "cache")
+    protected ConcurrentMap cache;
+
+    public void testCacheConfiguration() throws Exception {
+        assertNotNull(cache);
+    }
+
+    @Init
+    public void init() {
+        System.out.println("init");
+    }
+}
