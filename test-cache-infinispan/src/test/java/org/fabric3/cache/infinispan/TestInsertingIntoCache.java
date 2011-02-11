9ce80e702b7fe5ee5c4fe0b12ad379fbe0019743
michalcaposvn <noreply@fabric3.org>
2011-02-11 08:41:00 +0000

    - [integration test] updated test for inserting/reading object to/from cache

--- test-cache-infinispan/src/test/java/org/fabric3/cache/infinispan/TestInsertingIntoCache.java
+++ test-cache-infinispan/src/test/java/org/fabric3/cache/infinispan/TestInsertingIntoCache.java
@@ -2,6 +2,7 @@ package org.fabric3.cache.infinispan;
 
 import junit.framework.TestCase;
 import org.fabric3.api.MonitorChannel;
+import org.fabric3.api.annotation.Resource;
 import org.fabric3.api.annotation.monitor.Monitor;
 import org.fabric3.api.annotation.scope.Scopes;
 import org.oasisopen.sca.annotation.EagerInit;
@@ -20,16 +21,19 @@ import java.util.concurrent.ConcurrentMap;
 public class TestInsertingIntoCache extends TestCase {
 
     @Reference
-    protected AssertionService<Integer, String> AssertionService;
+    protected AssertionService<Integer, String> assertionService;
 
     @Reference
-    protected PublisherService<Integer, String> PublisherService;
+    protected PublisherService<Integer, String> publisherService;
 
     @Monitor
     protected MonitorChannel monitor;
 
+    @Resource(name = "dataIndexCache")
+    ConcurrentMap<Integer, String> cache;
+
     public void testCacheConfiguration() throws Exception {
-        assertTrue(AssertionService.assertCount(0));
+        assertTrue(assertionService.assertCount(0));
 
         int countToInsert = 10000;
 
@@ -40,11 +44,14 @@ public class TestInsertingIntoCache extends TestCase {
 
         long startDate = System.currentTimeMillis();
 
-        PublisherService.insertIntoCache(temp);
-        assertTrue(AssertionService.assertCount(countToInsert));
-        assertTrue(AssertionService.assertItems(temp));
+        publisherService.insertIntoCache(temp);
+        assertTrue(assertionService.assertCount(countToInsert));
+        assertTrue(assertionService.assertItems(temp));
 
         long endDate = System.currentTimeMillis();
         monitor.info(MessageFormat.format("Inserting of {0} items took: {1} ms", countToInsert, endDate - startDate));
+
+        cache.clear();
+        assertionService.assertCount(0);
     }
 }
