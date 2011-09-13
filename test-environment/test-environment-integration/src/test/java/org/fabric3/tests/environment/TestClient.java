ce281e7b540031fd75100ae72611312f24473233
jimmarino <jim.marino@gmail.com>
2011-09-13 11:59:41 +0000

    enable namespaces

--- test-environment/test-environment-integration/src/test/java/org/fabric3/tests/environment/TestClient.java
+++ test-environment/test-environment-integration/src/test/java/org/fabric3/tests/environment/TestClient.java
@@ -10,7 +10,16 @@ public class TestClient extends TestCase {
     @Reference
     protected TestService service;
 
-    public void testInvoke() {
+    @Reference
+    protected TestService systemConfigService;
+
+
+    public void testTemplateInComposite() {
         assertEquals("message", service.invoke("message"));
     }
+
+    public void testTemplateInSystemConfig() {
+        assertEquals("message", systemConfigService.invoke("message"));
+    }
+
 }
