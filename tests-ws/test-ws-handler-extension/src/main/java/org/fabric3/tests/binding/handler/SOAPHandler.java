6b30f7dc113621d6a82529dc95c634e9513685b0
jimmarino <jim.marino@gmail.com>
2012-02-22 00:54:26 +0000

    implement registration

--- tests-ws/test-ws-handler-extension/src/main/java/org/fabric3/tests/binding/handler/SOAPHandler.java
+++ tests-ws/test-ws-handler-extension/src/main/java/org/fabric3/tests/binding/handler/SOAPHandler.java
@@ -4,9 +4,13 @@ import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPMessage;
 
 import org.oasisopen.sca.Constants;
+import org.oasisopen.sca.annotation.Destroy;
 import org.oasisopen.sca.annotation.EagerInit;
+import org.oasisopen.sca.annotation.Init;
+import org.oasisopen.sca.annotation.Reference;
 
 import org.fabric3.spi.binding.handler.BindingHandler;
+import org.fabric3.spi.binding.handler.BindingHandlerRegistry;
 import org.fabric3.spi.invocation.Message;
 
 /**
@@ -15,6 +19,22 @@ import org.fabric3.spi.invocation.Message;
 @EagerInit
 public class SOAPHandler implements BindingHandler<SOAPMessage> {
     private static final QName BINDING = new QName(Constants.SCA_NS, "binding.ws");
+
+    private BindingHandlerRegistry registry;
+
+    public SOAPHandler(@Reference BindingHandlerRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Init
+    public void init() {
+        registry.register(this);
+    }
+
+    @Destroy
+    public void destroy() {
+        registry.unregister(this);
+    }
 
     public QName getType() {
         return BINDING;
