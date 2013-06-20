11c86efdd813ec99420a425816dba68560272f17
jmarino <jim.marino@gmail.com>
2013-06-20 11:58:20 +0200

    update dependencies

--- tests-apps/test-jms-client/src/main/java/org/fabric3/tests/jms/client/JmsClient.java
+++ tests-apps/test-jms-client/src/main/java/org/fabric3/tests/jms/client/JmsClient.java
@@ -1,16 +1,13 @@
 package org.fabric3.tests.jms.client;
 
-import java.net.InetAddress;
-import java.net.URI;
 import javax.jms.Connection;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
+import java.net.InetAddress;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.broker.TransportConnector;
 
 
 /**
@@ -21,16 +18,16 @@ public class JmsClient {
     public static void main(String[] args) throws Exception {
         String bindAddress = InetAddress.getLocalHost().getHostAddress();
 
-        BrokerService broker = new BrokerService();
-        broker.setUseJmx(true);
-        broker.setBrokerName("testClient");
-        TransportConnector connector = broker.addConnector("tcp://" + bindAddress + ":61617");
-        String group = "domain";
-        connector.setDiscoveryUri(URI.create("multicast://default?group=" + group));
-        broker.addNetworkConnector("multicast://default?group=" + group);
-        broker.start();
+//        BrokerService broker = new BrokerService();
+//        broker.setUseJmx(true);
+//        broker.setBrokerName("testClient");
+//        TransportConnector connector = broker.addConnector("tcp://" + bindAddress + ":61617");
+//        String group = "domain";
+//        connector.setDiscoveryUri(URI.create("multicast://default?group=" + group));
+//        broker.addNetworkConnector("multicast://default?group=" + group);
+//        broker.start();
 
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://" + bindAddress + ":61617");
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://" + bindAddress + ":61616");
         Connection connection = factory.createConnection();
         connection.start();
 
@@ -42,6 +39,6 @@ public class JmsClient {
         message.setText("text");
         producer.send(message);
         System.out.println("Done");
-        broker.stop();
+//        broker.stop();
     }
 }
